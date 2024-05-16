f293569f93be75189293ac8bcaae797d110ce8e4
Ast <93723066+YaboiAst@users.noreply.github.com>
2024-05-16 01:53:24 -0300

    Side Movement Done

--- Assets/NaveMovement.cs
+++ Assets/NaveMovement.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class NaveMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    [SerializeField] private Transform startPoint, endPoint;
+    [SerializeField] private float shipVelocity;
+    private int _direction;
+
+    private Vector3 GetDirection() => _direction == 1 ? startPoint.position : endPoint.position;
+    private void Start()
     {
-        
+        transform.position = startPoint.position;
+        _direction = -1;
+        MoveTowards();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveTowards()
     {
-        
+        transform.DOMoveX(GetDirection().x, shipVelocity)
+            .SetEase(Ease.Linear)
+            .OnComplete(UpdateDirection);
+    }
+
+    private void UpdateDirection()
+    {
+        var sequence = DOTween.Sequence();
+
+        sequence.AppendInterval(0.5f);
+        sequence.AppendCallback(() => _direction *= -1);
+        sequence.AppendCallback(MoveTowards);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(startPoint.position, endPoint.position);
     }
 }
