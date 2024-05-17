c4fb83fc601e9cd54639b784798c662853f48c6d
Naga <brunoyui@estudante.ufscar.br>
2024-05-17 10:39:57 -0300

    UIUI

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -2,58 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
    public GameObject fases; 
    public GameObject milkshake;
+   public GameObject blockmilkshake;
+   public Button milk;
    public GameObject kfc;
+   public GameObject blockkfc;
+   public Button kfcbutton;
    public GameObject sushi;
    public GameObject chocolate;
-  
+
+    public void Start()
+    {
+        milk.onClick.AddListener(Milkshake);
+        kfcbutton.onClick.AddListener(Kfc);   
+    }
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
     }
     public void Exit()
     {
         Application.Quit();
     }
-
     public void Terra()
     {
-    
        fases.SetActive(true);
-        
     }
      public void Viajar()
     {
         SceneManager.LoadScene("TesteMovimentoMG");
     }
-
+    
+    //CONTROLE DE BOTOES MILKSHAKE
      public void Milkshake()
     {
-            milkshake.SetActive(true);     
+        blockmilkshake.SetActive(true);     
     }
+     public void LiberaMilkshake()
+    {       
+        milk.onClick.RemoveListener(Milkshake);
+        milk.onClick.AddListener(()=>milkshake.SetActive(true));
+        blockmilkshake.SetActive(false);
+        milkshake.SetActive(true);       
+    }
+
+    //CONTROLE DE BOTOES KFC
     public void Kfc()
     {
-            kfc.SetActive(true);     
+        blockkfc.SetActive(true);     
+    }
+    public void LiberaKFC()
+    {       
+        kfcbutton.onClick.RemoveListener(Kfc);
+        kfcbutton.onClick.AddListener(()=>kfc.SetActive(true));
+        blockkfc.SetActive(false);
+        kfc.SetActive(true);       
     }
     public void Sushi()
     {
-            sushi.SetActive(true);     
+        sushi.SetActive(true);     
     }
      public void Chocolate()
     {
-            chocolate.SetActive(true);     
+        chocolate.SetActive(true);     
     }
     public void Fechar()
     {       
-            fases.SetActive(false);
-            milkshake.SetActive(false);
-            kfc.SetActive(false);
-            sushi.SetActive(false);
-            chocolate.SetActive(false);
+        fases.SetActive(false);
+
+        blockmilkshake.SetActive(false);
+        milkshake.SetActive(false); 
+
+        kfc.SetActive(false);
+        blockkfc.SetActive(false);
+            
+        sushi.SetActive(false);
+        chocolate.SetActive(false);
     }
 }
